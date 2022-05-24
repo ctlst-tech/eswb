c29d506ab146e4548b62103d66dcc58bbcef78e0
Ivan Makarov <ivan_makarov@icloud.com>
2022-05-24 21:06:53 +0400

    Implement draft of pyqt based display for eswb topics

--- pytools/eswb.py
+++ pytools/eswb.py
@@ -14,17 +14,20 @@ if sys == 'Linux':
 elif sys == 'Darwin':
     libname += '.dylib'
 else:
+
     raise "Not supported platform"
 
-libpath = '../cmake-build-debug/src/lib'
-fullpath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + libpath + os.path.sep + libname
+# libpath = '../cmake-build-debug/src/lib'
+# fullpath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + libpath + os.path.sep + libname
 
 try:
-    eswb = CDLL(fullpath)
-    libc = CDLL('libc.so')
+    eswb = CDLL(libname)
+    # eswb = CDLL(fullpath)
+    # libc = CDLL('libc.so')
     print("Successfully loaded ", eswb)
 except Exception as e:
     print(e)
+    print('Make sure lib is installed')
 
 
 eswb.eswb_strerror.restype = c_char_p
@@ -47,6 +50,82 @@ def eswb_exception(text, errcode = 0):
 
 def eswb_index(i: int):
     return c_uint32(i)
+
+
+def get_value_from_buf(topic_type, data_ref):
+    def castNvalue(ref, p_c_type):
+        return cast(ref, POINTER(p_c_type)).contents.value
+
+    none_stub = None
+
+    if topic_type == ce.tt_none:
+        return none_stub
+    elif topic_type == ce.tt_float:
+        return castNvalue(data_ref, c_float)
+    elif topic_type == ce.tt_double:
+        return castNvalue(data_ref, c_double)
+    elif topic_type == ce.tt_uint64:
+        return castNvalue(data_ref, c_uint64)
+    elif topic_type == ce.tt_int64:
+        return castNvalue(data_ref, c_int64)
+    elif topic_type == ce.tt_uint32:
+        return castNvalue(data_ref, c_uint32)
+    elif topic_type == ce.tt_int32:
+        return castNvalue(data_ref, c_int32)
+    elif topic_type == ce.tt_uint16:
+        return castNvalue(data_ref, c_uint16)
+    elif topic_type == ce.tt_int16:
+        return castNvalue(data_ref, c_int16)
+    elif topic_type == ce.tt_uint8:
+        return castNvalue(data_ref, c_uint8)
+    elif topic_type == ce.tt_int8:
+        return castNvalue(data_ref, c_int8)
+    elif topic_type == ce.tt_string:
+        return pstr(cast(data_ref, c_char_p).value)
+    elif topic_type == ce.tt_struct:
+        return none_stub
+    elif topic_type == ce.tt_fifo:
+        return none_stub
+    elif topic_type == ce.tt_byte_buffer:
+        return none_stub
+    elif topic_type == ce.tt_dir:
+        return none_stub
+    elif topic_type == ce.tt_event_queue:
+        return none_stub
+
+class TopicHandle:
+    def __init__(self, name: str, path: str):
+        self.name = name
+        self.path = path
+        self.td = c_int(0)
+        self.type = ce.tt_none
+        self.data_ref = pointer(c_int(20)) # FIXME
+
+    def connect(self):
+        c_td = c_int(0)
+        rv = eswb.eswb_subscribe(cstr(self.path), byref(c_td))
+        if rv != 0:
+            raise eswb_exception(f'eswb_subscribe to {self.path} failed', rv)
+        self.td = c_td
+
+        # eswb_rv_t
+        # eswb_get_topic_params(eswb_topic_descr_t td, topic_params_t * params);
+        topic_params = ce.topic_params_t()
+
+        rv = eswb.eswb_get_topic_params(self.td, byref(topic_params))
+        if rv != 0:
+            raise eswb_exception(f'eswb_get_topic_params failed', rv)
+
+        self.type = topic_params.type
+
+    def value(self):
+        rv = eswb.eswb_read(self.td, self.data_ref)
+        if rv != 0:
+            raise eswb_exception("eswb_read failed", rv)
+
+        return get_value_from_buf(self.type, self.data_ref)
+
+
 
 class Topic:
     def __init__(self, topic: POINTER(ce.topic_t)):
@@ -80,46 +159,7 @@ class Topic:
     # def iterator(self, call, n = None):
 
     def raw_value(self):
-        def castNvalue(ref, p_c_type):
-            return cast(ref, POINTER(p_c_type)).contents.value
-
-        none_stub = None
-
-        if self.type == ce.tt_none:
-            return none_stub
-        elif self.type == ce.tt_float:
-            return castNvalue(self.data_ref, c_float)
-        elif self.type == ce.tt_double:
-            return castNvalue(self.data_ref, c_double)
-        elif self.type == ce.tt_uint64:
-            return castNvalue(self.data_ref, c_uint64)
-        elif self.type == ce.tt_int64:
-            return castNvalue(self.data_ref, c_int64)
-        elif self.type == ce.tt_uint32:
-            return castNvalue(self.data_ref, c_uint32)
-        elif self.type == ce.tt_int32:
-            return castNvalue(self.data_ref, c_int32)
-        elif self.type == ce.tt_uint16:
-            return castNvalue(self.data_ref, c_uint16)
-        elif self.type == ce.tt_int16:
-            return castNvalue(self.data_ref, c_int16)
-        elif self.type == ce.tt_uint8:
-            return castNvalue(self.data_ref, c_uint8)
-        elif self.type == ce.tt_int8:
-            return castNvalue(self.data_ref, c_int8)
-        elif self.type == ce.tt_string:
-            return pstr(cast(self.data_ref, c_char_p).value)
-        elif self.type == ce.tt_struct:
-            return none_stub
-        elif self.type == ce.tt_fifo:
-            return none_stub
-        elif self.type == ce.tt_byte_buffer:
-            return none_stub
-        elif self.type == ce.tt_dir:
-            return none_stub
-        elif self.type == ce.tt_event_queue:
-            return none_stub
-
+        return get_value_from_buf(self.type, self.data_ref)
 
     def print(self, show_types = False):
         def print_node(t, indent = 0):
--- pytools/eswbmon.py
+++ pytools/eswbmon.py
@@ -1,19 +1,123 @@
 #!/usr/bin/env python3
 
-# https://github.com/pyqt/examples
+# TODO https://github.com/pyqt/examples
 
-# Planned use cases:
-# 1. connect/disconnect via available tools:
-#   - tcp
-#   - serial
-#   - file
-# 1.1 handle multiple busses connectivity
-# 2. monitor values in a tree
-#   - extent
-#   - collapse
-#   - see properties
-#   - pin params and whole branches
-# 3. create plots with running window via selecting params in tree
-# 4. save / restore layout and plots
-#     - able to ask for a template from command line
-# 5. highlight params with fading over time color according to its "freshness"
+from abc import abstractmethod
+from time import sleep
+from typing import List
+
+from PyQt5 import QtWidgets, QtCore
+import pyqtgraph as pg
+from random import randint
+
+import eswb as e
+
+class ewBasic(QtWidgets.QWidget):
+    def __init__(self,name, path, *args, **kwargs):
+        super(ewBasic, self).__init__(*args, **kwargs)
+
+        self.topics = []
+        self.topics.append(e.TopicHandle(name, path))
+
+    @abstractmethod
+    def update_handler(self, vals: List[float]):
+        pass
+
+    def connect(self):
+        for t in self.topics:
+            t.connect()
+
+    def update(self):
+        vals = []
+        for t in self.topics:
+            vals.append(t.value())
+
+        self.update_handler(vals)
+
+
+class ewChart(ewBasic):
+    def __init__(self, *args, **kwargs):
+        super(ewChart, self).__init__(*args, **kwargs)
+        self.graph = pg.PlotWidget()
+
+        layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight)
+        layout.addWidget(self.graph)
+
+        self.x = list(range(100))
+        self.y = [randint(0, 100) for _ in range(100)]
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line = self.graph.plot(self.x, self.y, pen=pen)
+
+        self.setLayout(layout)
+
+    def update_handler(self, vals: List[float]):
+
+        self.x = self.x[1:]
+        self.x.append(self.x[-1] + 1)
+
+        self.y = self.y[1:]
+        self.y.append(int(vals[0]))
+
+        self.data_line.setData(self.x, self.y)
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.widgets = []
+
+        # self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("ESWB display")
+
+        self.main_widget = QtWidgets.QWidget(self)
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.main_layout = QtWidgets.QVBoxLayout(self.main_widget)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(20)
+        self.timer.timeout.connect(self.redraw)
+        self.timer.start()
+
+    def add_ew(self, widget):
+        self.main_layout.addWidget(widget)
+        self.widgets.append(widget)
+
+    def connect(self):
+        for w in self.widgets:
+            w.connect()
+
+    def redraw(self):
+        for w in self.widgets:
+            w.update()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    eswb_display = ApplicationWindow()
+
+    b = e.Bus('service')
+    b.mkdir('vehicle')
+
+    client = e.EQRBtcp('itb:/vehicle', f'nsb:/service/vehicle')
+    client.connect('127.0.0.1')
+
+    eswb_display.add_ew(ewChart(name='rpm', path='nsb:/service/vehicle/engine/rpm'))
+    eswb_display.add_ew(ewChart(name='cht', path='nsb:/service/vehicle/engine/cht'))
+    eswb_display.add_ew(ewChart(name='egt', path='nsb:/service/vehicle/engine/egt'))
+    eswb_display.add_ew(ewChart(name='oil_press', path='nsb:/service/vehicle/engine/oil_press'))
+
+    eswb_display.show()
+
+    sleep(0.2)
+
+    b.update_tree()
+    t = b.get_topics_tree()
+
+    eswb_display.connect()
+
+    sys.exit(app.exec_())
