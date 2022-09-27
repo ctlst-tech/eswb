17f1f282935de59d597fea0a098aa45d89232355
Ivan Makarov <ivan_makarov@icloud.com>
2022-09-27 19:55:10 -0400

    Reimplement eswbutil cpp tool for SDTL based bridging

--- cpp/eswb.hpp
+++ cpp/eswb.hpp
@@ -6,6 +6,7 @@
 #include <eswb/api.h>
 #include <eswb/event_queue.h>
 #include "eswb/services/eqrb.h"
+#include "eswb/services/sdtl.h"
 
 namespace eswb {
 
@@ -151,63 +152,100 @@ public:
 };
 
 
-class Bridge {
-    std::string bus2replicate;
-    std::string replicate_to;
-    struct eqrb_client_handle *ch;
+class sdtl {
+
+    sdtl_service_t *service;
+    const std::string service_name;
+    size_t mtu;
+    std::string media_name;
 
 public:
-    Bridge(const std::string &bus2replicate_, const std::string &replicate_to_) :
-            bus2replicate(bus2replicate_),
-            replicate_to(replicate_to_) {
+    sdtl(const std::string &sname, size_t mtu_) :
+        service_name(sname), mtu(mtu_){
 
-        eqrb_rv_t rv = eqrb_tcp_client_create(&ch);
-
-        if (rv != eqrb_rv_ok) {
-            throw Exception("eqrb_tcp_client_create (fix code)", eswb_e_invargs);
-        }
+        media_name = "serial";
     }
 
-    void connect(const std::string &addr, int repl_map_size = 1024) {
-        char err_msg[256];
-
-        eqrb_rv_t rv = eqrb_tcp_client_connect(ch, addr.c_str(),
-                                               bus2replicate.c_str(),
-                                               replicate_to.c_str(),
-                                               repl_map_size, err_msg);
-
-        if (rv != eqrb_rv_ok) {
-            std::string msg = "eqrb_tcp_client_connect (fix code)" + std::string(err_msg);
-            throw Exception(msg, eswb_e_invargs);
-        }
+    sdtl_rv_t init(const std::string &mount_point) {
+        return sdtl_service_init_w(&service, service_name.c_str(), mount_point.c_str(), mtu, 16, media_name.c_str());
     }
 
-    void close() {
-        eqrb_tcp_client_close(ch);
+
+    sdtl_rv_t add_channel(const std::string &ch_name, uint8_t id, sdtl_channel_type_t type) {
+        sdtl_channel_cfg_t cfg;
+
+        cfg.name = ch_name.c_str();
+        cfg.id = id;
+        cfg.type = type;
+        cfg.mtu_override = 0;
+
+        return sdtl_channel_create(service, &cfg);
+    }
+
+    sdtl_rv_t start(const std::string &dev_path, uint32_t baudrate) {
+        sdtl_media_serial_params_t params;
+        params.baudrate = baudrate;
+        return sdtl_service_start(service, dev_path.c_str(), &params);
     }
 };
 
 
-class BridgeSerial {
+class BridgeSDTL {
     std::string replicate_to;
+    std::string device_path;
+    uint32_t baudrate;
+
+    const std::string bus_name;
+    const std::string service_name;
+
+    sdtl sdtl_service;
+    Bus sdtl_service_bus;
+
+
+    const struct {
+        std::string ch_name;
+        uint8_t id;
+        sdtl_channel_type_t type;
+    } ch_cfgs [2] = {
+        {
+            .ch_name = "bus_sync",
+            .id = 1,
+            .type = SDTL_CHANNEL_RELIABLE
+        },
+        {
+            .ch_name = "bus_sync_sk",
+            .id = 2,
+            .type = SDTL_CHANNEL_UNRELIABLE
+        }
+    };
 
 public:
-    BridgeSerial(const std::string &replicate_to_) :
-            replicate_to(replicate_to_) {
+    BridgeSDTL(const std::string &dp, uint32_t br, const std::string &replicate_to_):
+            replicate_to(replicate_to_),
+            device_path(dp),
+            baudrate(br),
+            bus_name("sdtl_bus"),
+            service_name("sdtl"),
+            sdtl_service(service_name, 0),
+            sdtl_service_bus(bus_name, inter_thread, 256) {
 
+        sdtl_service.init(bus_name);
+
+        sdtl_service.add_channel(ch_cfgs[0].ch_name, ch_cfgs[0].id, ch_cfgs[0].type);
+        sdtl_service.add_channel(ch_cfgs[1].ch_name, ch_cfgs[1].id, ch_cfgs[1].type);
     }
 
-    void connect(const std::string &path, int baudrate, int repl_map_size = 1024) {
-        char err_msg[256];
-
-        eqrb_rv_t rv = eqrb_serial_client_connect(path.c_str(), baudrate,
-                                               replicate_to.c_str(),
-                                               repl_map_size);
-
-        if (rv != eqrb_rv_ok) {
-            std::string msg = "eqrb_serial_client_connect (fix code)" + std::string(err_msg);
-            throw Exception(msg, eswb_e_invargs);
+    eqrb_rv_t start() {
+        sdtl_rv_t srv = sdtl_service.start(device_path, baudrate);
+        if (srv != SDTL_OK) {
+            return eqrb_media_err;
         }
+
+        return eqrb_sdtl_client_connect(service_name.c_str(),
+                                 ch_cfgs[0].ch_name.c_str(), ch_cfgs[1].ch_name.c_str(),
+                                 replicate_to.c_str(),
+                                 1024);
+
     }
 };
 
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,23 +10,20 @@ void clrsrc () {
 }
 
 int main(int argc, char* argv[]) {
-    eswb::Bus service_bus("service", eswb::inter_thread, 2048);
+    std::string monitor = "monitor";
+    eswb::Bus monitor_bus(monitor, eswb::inter_thread, 2048);
 
-    std::string bus2request = "conversions";
+    std::string bus2request = "sens";
 
-    auto p = service_bus.mkdir(bus2request);
+    monitor_bus.mkdir(bus2request);
 
-//    eswb::Bridge br("itb:/" + bus2request, p);
-//    br.connect("127.0.0.1");
-
-
-    eswb::BridgeSerial br("itb:/ + bus2request");
-    br.connect("/dev/tty.usbserial-0001", 115200);
+    auto sdtl_bridge = eswb::BridgeSDTL("/tmp/vserial2", 115200, monitor + "/" + bus2request);
+    sdtl_bridge.start();
 
     while(1) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        service_bus.update_tree();
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        monitor_bus.update_tree();
         clrsrc();
-        service_bus.print_tree();
+        monitor_bus.print_tree();
     }
 }
