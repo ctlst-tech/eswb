0e4a391c0b6b7a5b47a5ffea576c53c047c3a6ac
Ivan Makarov <ivan_makarov@icloud.com>
2022-12-08 22:03:27 +0400

    Add error check for sdtl_bridge.start() in eswbutil

--- cpp/eswbutil.cpp
+++ cpp/eswbutil.cpp
@@ -106,7 +106,11 @@ void read_sdtl_bridge_serial(const std::string &path, unsigned baudrate) {
     monitor_bus.mkdir(bus2request);
 
     auto sdtl_bridge = eswb::BridgeSDTL(path, baudrate, monitor + "/" + bus2request);
-    sdtl_bridge.start();
+    auto rv = sdtl_bridge.start();
+    if (rv != eqrb_rv_ok){
+        std::cerr << "sdtl_bridge start error: " << eqrb_strerror(rv) << std::endl;
+        return;
+    }
 
     while(1) {
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
