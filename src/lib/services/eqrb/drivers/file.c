85ed58317c82e95e808ec9b772bc006164552933
Ivan Solodilov <optim000000@gmail.com>
2022-12-21 18:23:03 +0400

    Add connect and send to file eqrb driver

--- src/lib/services/eqrb/drivers/file.c
+++ src/lib/services/eqrb/drivers/file.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -25,3 +26,31 @@ const eqrb_media_driver_t eqrb_drv_file = {
     .check_state = eqrb_drv_file_check_state,
     .disconnect = eqrb_drv_file_disconnect,
 };
+
+eqrb_rv_t eqrb_drv_file_connect(void *param, device_descr_t *dh) {
+    eqrb_drv_file_params_t *p = (eqrb_drv_file_params_t *)param;
+
+    if (dh == NULL) {
+        return eqrb_media_invarg;
+    }
+
+    *(int *)dh = open("gf.txt", O_RDWR | O_CREAT | O_TRUNC);
+
+    return *(int *)dh > 0 ? eqrb_rv_ok : eqrb_media_err;
+}
+
+eqrb_rv_t eqrb_drv_file_send(device_descr_t dh, void *data, size_t bts,
+                             size_t *bs) {
+    size_t bw = write((int)dh, data, bts);
+
+    if (bw > 0) {
+        if (bs != NULL) {
+            *bs = bw;
+        }
+        return eqrb_rv_ok;
+    } else if (bw == 0) {
+        return eqrb_media_stop;
+    }
+
+    return eqrb_media_err;
+}
