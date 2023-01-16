9fa9d447f4c840c88521f6e0342ce0015fc20f9c
Ivan Solodilov <optim000000@gmail.com>
2023-01-17 01:02:10 +0400

    Add two topics with two vars, add bus name to file drv struct

--- src/lib/services/eqrb/drivers/file.c
+++ src/lib/services/eqrb/drivers/file.c
@@ -15,6 +15,7 @@ typedef struct {
     const char *file_prefix;
     const char *file_name;
     const char *dir;
+    const char *bus;
     struct timespec tp;
 } eqrb_drv_file_params_t;
 
@@ -26,7 +27,8 @@ eqrb_rv_t eqrb_drv_file_recv(device_descr_t dh, void *data, size_t bts,
 eqrb_rv_t eqrb_drv_file_command(device_descr_t dh, eqrb_cmd_t cmd);
 eqrb_rv_t eqrb_drv_file_check_state(device_descr_t dh);
 eqrb_rv_t eqrb_drv_file_disconnect(device_descr_t dh);
-eqrb_rv_t eqrb_drv_file_update_timestamp_us(device_descr_t dh, uint32_t *ts, uint32_t *dts);
+eqrb_rv_t eqrb_drv_file_update_timestamp_us(device_descr_t dh, uint32_t *ts,
+                                            uint32_t *dts);
 
 const eqrb_media_driver_t eqrb_drv_file = {
     .name = "eqrb_file",
@@ -73,9 +75,17 @@ eqrb_rv_t eqrb_drv_file_connect(void *param, device_descr_t *dh) {
     if (fd > 0 && fd < EQRB_FILE_MAX_DESC) {
         file_params[fd] = param;
         *(int *)dh = fd;
-        rv = eqrb_rv_ok;
+        const char bus[32];
+        int bus_len = snprintf(bus, sizeof(bus), "%s", p->bus);
+        size_t bw = write(fd, bus, bus_len);
+        sync();
+        if (bw > 0) {
+            rv = eqrb_rv_ok;
+        } else {
+            rv = eqrb_media_err;
+        }
     } else {
-        rv = eqrb_media_err;
+        rv = eqrb_media_invarg;
     }
 
     return rv;
@@ -88,7 +98,8 @@ eqrb_rv_t eqrb_drv_file_send(device_descr_t dh, void *data, size_t bts,
     uint32_t t, dt;
 
     eqrb_drv_file_update_timestamp_us(dh, &t, &dt);
-    head_time_len = snprintf(head_time, sizeof(head_time), "%s%f\n", head, t / 1000000.0);
+    head_time_len =
+        snprintf(head_time, sizeof(head_time), "\n%s%f", head, t / 1000000.0);
 
     size_t bw = write((int)dh, head_time, head_time_len);
     sync();
@@ -140,7 +151,8 @@ eqrb_rv_t eqrb_drv_file_disconnect(device_descr_t dh) {
 }
 
 static eqrb_rv_t init_media_params(void **media_params, const char *file_prefix,
-                                   const char *file_name, const char *dir) {
+                                   const char *file_name, const char *dir,
+                                   const char *bus) {
     eqrb_drv_file_params_t *params = calloc(1, sizeof(*params));
 
     if (params == NULL) {
@@ -159,7 +171,18 @@ static eqrb_rv_t init_media_params(void **media_params, const char *file_prefix,
         params->file_name = NULL;
     }
 
-    params->dir = strdup(dir);
+    if (bus != NULL) {
+        params->bus = strdup(bus);
+    } else {
+        params->bus = NULL;
+    }
+
+    if (dir != NULL) {
+        params->dir = strdup(dir);
+    } else {
+        params->dir = NULL;
+    }
+
     *media_params = params;
 
     return eqrb_rv_ok;
@@ -173,7 +196,7 @@ eqrb_rv_t eqrb_file_server_start(const char *eqrb_service_name,
     eqrb_rv_t rv;
     eqrb_server_handle_t *sh;
 
-    rv = init_media_params(&mp, file_prefix, NULL, dst_dir);
+    rv = init_media_params(&mp, file_prefix, NULL, dst_dir, bus2replicate);
     if (rv != eqrb_rv_ok) {
         return rv;
     }
@@ -228,7 +251,8 @@ eqrb_rv_t eqrb_file_client_connect(const char *service_name,
     return eqrb_rv_ok;
 }
 
-eqrb_rv_t eqrb_drv_file_update_timestamp_us(device_descr_t dh, uint32_t *ts, uint32_t *dts) {
+eqrb_rv_t eqrb_drv_file_update_timestamp_us(device_descr_t dh, uint32_t *ts,
+                                            uint32_t *dts) {
     int fd = (int)dh;
 
     if (ts == NULL || file_params[fd] == NULL) {
@@ -236,11 +260,11 @@ eqrb_rv_t eqrb_drv_file_update_timestamp_us(device_descr_t dh, uint32_t *ts, uin
     }
 
     struct timespec *tp = &file_params[fd]->tp;
-    uint32_t prev = tp->tv_sec * 1000000 + tp->tv_nsec / 1000; 
+    uint32_t prev = tp->tv_sec * 1000000 + tp->tv_nsec / 1000;
     clock_gettime(CLOCK_REALTIME, &file_params[fd]->tp);
     uint32_t current = tp->tv_sec * 1000000 + tp->tv_nsec / 1000;
     *ts = current;
-    *dts = current - prev; 
+    *dts = current - prev;
 
     return eqrb_rv_ok;
 }
--- tests/eqrb_file.cpp
+++ tests/eqrb_file.cpp
@@ -1,8 +1,7 @@
-// #include <catch2/catch_all.hpp>
 #include <pthread.h>
 
+// #include <catch2/catch_all.hpp>
 #include <cmath>
-#include <cstdio>
 #include <cstring>
 #include <string>
 
@@ -16,31 +15,37 @@ public:
     std::string pub;
     std::string bus;
     eswb_topic_descr_t topic_desc;
-    eswb_topic_descr_t queue_desc;
-    eswb_topic_descr_t bus_desc;
     virtual int init(void) = 0;
     virtual int update(void) = 0;
 };
 
 class SinGenerator final : public BasePublisher {
 private:
-    struct out {
-        double v;
-    } m_out;
+    struct SinGen {
+        double in;
+        double out;
+    } m_sin;
+    double m_amp;
+    double m_period_scaler;
     uint64_t m_counter;
 
 public:
-    SinGenerator(const std::string &pub_name, const std::string &bus_name)
-        : m_counter(0) {
+    SinGenerator(const std::string &pub_name, const std::string &bus_name,
+                 int amp, double period_scaler)
+        : m_amp(amp), m_period_scaler(period_scaler), m_counter(0) {
         pub = std::move(pub_name);
         bus = std::move(bus_name);
     }
 
     virtual int init(void) final {
         TOPIC_TREE_CONTEXT_LOCAL_DEFINE(cntx, 10);
-        topic_proclaiming_tree_t *rt = usr_topic_set_struct(cntx, m_out, "sin");
-        usr_topic_add_struct_child(cntx, rt, struct out, v, "out", tt_double);
 
+        topic_proclaiming_tree_t *rt =
+            usr_topic_set_struct(cntx, m_sin, pub.c_str());
+        usr_topic_add_struct_child(cntx, rt, struct SinGen, in, "in",
+                                   tt_double);
+        usr_topic_add_struct_child(cntx, rt, struct SinGen, out, "out",
+                                   tt_double);
         eswb_rv_t rv = eswb_proclaim_tree_by_path(
             this->bus.c_str(), rt, cntx->t_num, &this->topic_desc);
         // REQUIRE(rv == eswb_e_ok);
@@ -49,11 +54,12 @@ public:
     }
 
     virtual int update(void) final {
-        struct out out;
-        out.v = sin(m_counter * 0.05);
-        eswb_update_topic(topic_desc, &out);
+        struct SinGen sin_data;
+        sin_data.in = m_counter * m_period_scaler;
+        sin_data.out = sin(sin_data.in) * m_amp;
+        eswb_update_topic(topic_desc, &sin_data);
         m_counter++;
-        sleep(1);
+        usleep(1000);
         return 0;
     }
 };
@@ -70,26 +76,32 @@ class EqrbFileTest {
 public:
     EqrbFileTest() = default;
 
-    int startPublisherService(BasePublisher *publisher) {
+    int createBus(const std::string &bus) {
         eswb_rv_t rv;
+        eswb_topic_descr_t queue_desc;
+        eswb_topic_descr_t bus_desc;
 
-        rv = eswb_create(publisher->bus.c_str(), eswb_inter_thread, 20);
+        rv = eswb_create(bus.c_str(), eswb_inter_thread, 20);
         // REQUIRE(rv == eswb_e_ok);
 
-        rv = eswb_connect(publisher->bus.c_str(), &publisher->bus_desc);
+        rv = eswb_connect(bus.c_str(), &bus_desc);
         // REQUIRE(rv == eswb_e_ok);
 
-        rv = eswb_event_queue_enable(publisher->bus_desc, 40, 1024);
+        rv = eswb_event_queue_enable(bus_desc, 40, 1024);
         // REQUIRE(erv == eswb_e_ok);
 
-        // rv = eswb_event_queue_subscribe(publisher->bus.c_str(), &publisher->queue_desc);
+        // rv = eswb_event_queue_subscribe(publisher->bus.c_str(),
+        // &publisher->queue_desc); REQUIRE(erv == eswb_e_ok);
+
+        // rv = eswb_event_queue_set_receive_mask(publisher->queue_desc,
+        // 0xFFFFFFFF); REQUIRE(erv == eswb_e_ok);
+        rv = eswb_event_queue_order_topic(bus_desc, bus.c_str(), 1);
         // REQUIRE(erv == eswb_e_ok);
 
-        // rv = eswb_event_queue_set_receive_mask(publisher->queue_desc, 0xFFFFFFFF);
-        // REQUIRE(erv == eswb_e_ok);
-        rv = eswb_event_queue_order_topic(publisher->bus_desc, publisher->bus.c_str(), 1);
-        // REQUIRE(erv == eswb_e_ok);
+        return 0;
+    }
 
+    int startPublisherService(BasePublisher *publisher) {
         pthread_t tid;
         pthread_create(&tid, NULL, publisherThread, (void *)publisher);
         // REQUIRE(tid != NULL);
@@ -125,13 +137,16 @@ public:
 
 int main(int argc, char **argv) {
     EqrbFileTest eqrb_test;
-    BasePublisher *publisher =
-        new SinGenerator(std::string("sin"), std::string("generators"));
+    BasePublisher *sin1 = new SinGenerator(std::string("sin1"),
+                                           std::string("generators"), 1.0, 1.0);
+    BasePublisher *sin2 = new SinGenerator(std::string("sin2"),
+                                           std::string("generators"), 2.0, 2.0);
 
-    eqrb_test.startPublisherService(publisher);
+    eqrb_test.createBus(std::string("generators"));
+    eqrb_test.startPublisherService(sin1);
+    eqrb_test.startPublisherService(sin2);
     eqrb_test.startEqrbService("eqrb_file_write_service", "generators", "sin",
                                "/tmp");
-
     while (1) {
         sleep(1000);
     }
