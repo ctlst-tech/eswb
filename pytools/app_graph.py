9a3bf467ff0208e5b6eb6c8821dadfa828f1442e
Aleksei Kudriashov <akud.soft@gmail.com>
2023-01-27 14:18:35 +0200

    Add app_graph demo

--- pytools/app_graph.py
+++ pytools/app_graph.py
@@ -3,6 +3,7 @@ from controls import *
 from controls.datasources import *
 from controls.graph3d import EwGraph3D
 from monitor import Monitor
+from relative_position import EwRelativePosition
 
 mon_bus_name = 'monitor'
 telemetry_dir_name = 'telemetry'
@@ -17,6 +18,20 @@ g3d = EwGraph3D([
     DataSourceSinus('s3', iphase=0.6, mult=10.)
 ])
 
-mon.add_widget(EwGroup([hi, g3d]))
+ai = EwAttitudeIndicator([
+    DataSourceSinus('s1', iphase=0.0, mult=45),
+    DataSourceSinus('s2', iphase=1.0, mult=20)
+])
+
+rp = EwRelativePosition([
+    DataSourceSinus('plane_phi', mult=360),
+    DataSourceConst('plane_r', value=60),
+    DataSourceSinus('plane_course', iphase=0.0, mult=360),
+
+    DataSourceSinus('base_phi', iphase=0.5, mult=360),
+    DataSourceConst('base_r', value=120),
+])
+
+mon.add_widget(EwGroup([rp, ai, hi, g3d]))
 
 mon.run()
