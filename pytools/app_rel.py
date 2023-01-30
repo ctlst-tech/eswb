a780434ed73c00ce86fee08c2f91b7649b0cfbf9
Aleksei Kudriashov <akud.soft@gmail.com>
2023-01-30 23:44:02 +0200

    Add relative position scaling and sizes

---
+++ pytools/app_rel.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from ds import *
+from ew import *
+from monitor import Monitor
+
+mon_bus_name = 'monitor'
+telemetry_dir_name = 'telemetry'
+
+mon = Monitor(monitor_bus_name=mon_bus_name, argv=sys.argv)
+
+rp = EwRelativePosition([
+    DataSourceSinus('plane_phi', mult=360),
+    DataSourceConst('plane_r', value=60),
+    DataSourceSinus('plane_course', iphase=0.0, mult=360),
+
+    DataSourceSinus('base_phi', iphase=0.5, mult=360),
+    DataSourceConst('base_r', value=120),
+
+    DataSourceSinus('target_phi', iphase=-0.5, mult=360),
+    DataSourceConst('target_r', value=160),
+])
+
+mon.add_widget(EwGroup([rp]))
+
+mon.app_window.resize(800, 800)
+
+mon.run()
