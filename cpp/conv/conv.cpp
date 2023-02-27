6d00d626e5670110600ff254946308f44ef07dbf
Ivan Solodilov <optim000000@gmail.com>
2023-02-27 20:15:01 +0400

    Add structs, parse raw

--- cpp/conv/conv.cpp
+++ cpp/conv/conv.cpp
@@ -1,6 +1,25 @@
 #include "conv.hpp"
 
+#include <cstring>
 #include <fstream>
+
+char *eswb::ConverterToCsv::findSep(char *str, char *end_ptr, const char *sep) {
+    char *rv;
+    int length;
+    while (1) {
+        if (str >= end_ptr) {
+            return nullptr;
+        }
+        rv = strstr(str, sep);
+        length = strnlen(str, end_ptr - str);
+        length = (length < 1 ? 1 : length);
+        if (rv == nullptr) {
+            str += length;
+        } else {
+            return rv;
+        }
+    }
+}
 
 int eswb::ConverterToCsv::convert(void) {
     int rv;
@@ -26,12 +45,61 @@ int eswb::ConverterToCsv::convert(void) {
     std::getline(raw_log, this->m_bus);
     std::cout << "Bus: " << this->m_bus << std::endl;
 
-    while (std::getline(raw_log, raw_string)) {
-        
+    raw_log.seekg(0, std::ios::end);
+    size_t raw_length = raw_log.tellg();
+    raw_log.seekg(0, std::ios::beg);
+    char *raw_buf = new char[raw_length];
+
+    if (raw_buf == nullptr) {
+        std::cout << "Failed to create raw buf: " << std::endl;
+    }
+
+    raw_log.read(raw_buf, raw_length);
+    char *raw_end_ptr = raw_buf + raw_length;
+
+    bool loop = true;
+    char *topic_start = raw_buf;
+    char *topic_end = raw_buf;
+    int topic_length;
+
+    eswb::eqrb_interaction_header_t *h;
+    eswb::event_queue_transfer_t *event;
+    char timestamp[32];
+    char *data;
+
+    while (1) {
+        topic_start = findSep(topic_start, raw_end_ptr, m_frame_sep.data());
+        if (topic_start == nullptr) {
+            break;
+        }
+        topic_end = topic_start + m_frame_sep.length();
+        topic_end = findSep(topic_end, raw_end_ptr, m_frame_sep.data());
+        if (topic_end == nullptr) {
+            topic_end = raw_end_ptr;
+        }
+        topic_length = topic_end - topic_start;
+        topic_start = topic_start + m_frame_sep.length();
+        sscanf(topic_start, "%[0-9.]", timestamp);
+        topic_start += strlen(timestamp);
+
+        h = (eswb::eqrb_interaction_header_t *)topic_start;
+        event = (eswb::event_queue_transfer_t
+                     *)(topic_start + sizeof(eswb::eqrb_interaction_header_t));
+        data = (char *)event + sizeof(eswb::event_queue_transfer_t);
+        std::cout << "msg_code: " << (uint32_t)h->msg_code << std::endl; 
+        std::cout << "size:     " << event->size << std::endl; 
+        std::cout << "topic_id: " << event->topic_id << std::endl; 
+        std::cout << "type:     " << (uint32_t)event->type << std::endl;
+        std::cout << "data:     " << (char *)data << std::endl;
+        std::cout << std::endl; 
+
+        if (h->msg_code == eswb::EQRB_CMD_SERVER_TOPIC) {
+        }
     }
 
     raw_log.close();
     csv_log.close();
+    delete[] raw_buf;
 
     return rv;
 }
--- cpp/conv/conv.hpp
+++ cpp/conv/conv.hpp
@@ -3,12 +3,34 @@
 
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <unordered_map>
 
 #include "eswb.hpp"
 #include "eswb/services/eqrb.h"
 #include "eswb/services/sdtl.h"
 
 namespace eswb {
+
+// TODO: use headers with definitions
+typedef struct  __attribute__((packed)) eqrb_interaction_header {
+    uint8_t msg_code;
+    uint8_t reserved[3];
+} eqrb_interaction_header_t;
+
+typedef struct  __attribute__((packed)) event_queue_transfer {
+    uint32_t size;
+    uint32_t topic_id;
+    uint8_t  type;
+ /* uint8_t  data[size]; */
+} event_queue_transfer_t;
+
+typedef enum {
+    EQRB_CMD_CLIENT_REQ_SYNC = 0,
+    EQRB_CMD_CLIENT_REQ_STREAM = 1,
+    EQRB_CMD_SERVER_EVENT = 2,
+    EQRB_CMD_SERVER_TOPIC = 3,
+} eqrb_cmd_code_t;
 
 class ConverterToCsv {
 private:
@@ -18,6 +40,11 @@ private:
 
 private:
     std::string m_bus;
+
+private:
+    std::vector<std::string> header;
+    std::vector<std::string> raw;
+    std::unordered_map<int, std::string> hash;
 
 public:
     ConverterToCsv(const std::string &path_to_raw,
@@ -31,6 +58,9 @@ public:
 
 public:
     int convert(void);
+
+private:
+    char *findSep(char *str, char *end_ptr, const char *sep);
 };
 
 }  // namespace eswb
